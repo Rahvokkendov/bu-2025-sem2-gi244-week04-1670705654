b5c930311226b61b733a47d645d666a1ad2daba4
Rahvokkendov <rahvokkendov@gmail.com>
2026-02-06 16:24:45 +0700

    Dog Spawn 1st Ver

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,10 +1,23 @@
+using NUnit.Framework;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    [SerializeField] private GameObject[] dogPrefabs;
+
     // Update is called once per frame
     void Update()
     {
-
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            float x = Random.Range(-10,10);
+            int dogIndex = Random.Range(0,dogPrefabs.Length);
+            if (dogPrefabs[dogIndex] != null)
+            {
+                Instantiate(dogPrefabs[dogIndex], new Vector3(x,0,20), Quaternion.Euler(0,180,0));
+            }
+            
+        }
+        
     }
 }
