1b5b71aac927fb7c3e3fff8e3de18802261cdd79
Rahvokkendov <rahvokkendov@gmail.com>
2026-02-06 15:46:12 +0700

    Functional Player Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,14 +3,46 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 10.0f;
+    public float xRange = 10;
+    private InputAction moveAction;
+
     private void Awake()
     {
-
+         moveAction = InputSystem.actions.FindAction("Move");
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+        var hInput = moveAction.ReadValue<Vector2>().x;
+        transform.Translate(hInput * speed * Time.deltaTime * Vector3.right);
+
+        if (transform.position.x < -xRange )
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x > xRange )
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        //Gizmos.DrawWireSphere(transform.position, 1f);
+        //Gizmos.color = Color.red;
+        // Gizmos.DrawLine(transform.position, Camera.main.transform.position);
+        Vector3 left = new Vector3(-xRange, transform.position.y, transform.position.z);
+        Vector3 right = new Vector3(xRange, transform.position.y, transform.position.z);
+        Gizmos.DrawLine(left, right);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(left, 1f);
+        Gizmos.DrawWireSphere(right, 1f);
 
     }
 }
